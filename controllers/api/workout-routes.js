9c6fe002367464d4177b4c613e33e66d9eee5bfd
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-06-05 18:28:48 -0400

    added routes

---
+++ controllers/api/workout-routes.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const { Workout } = require("../../models");
+
+router.post("/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.create(req.body);
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/workouts/:id", async (req, res) => {
+  try {
+    const workoutData = await Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    );
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]);
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/workouts/range", async (req, res) => {
+  try {
+    const workoutData = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]);
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.findByIdAndDelete(req.body);
+    res.status(200).json(true);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
